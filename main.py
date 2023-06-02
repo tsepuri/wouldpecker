c4e5a847e51b2b5e56d775cfc4a868c44c3db7a1
shailstorm <shailstorms@gmail.com>
2023-06-02 16:14:27 -0400

    fixing privacy concern

--- main.py
+++ main.py
@@ -91,7 +91,6 @@ class TwilioManager:
 				to=to,
 				body=body,
 				from_=self.FROM_NUMBER,
-				# media_url=['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg']
 			)
 
 	def on_sms_received(self, number: str, body: str) -> None: raise NotImplemented
